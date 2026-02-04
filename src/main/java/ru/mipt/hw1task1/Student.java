9c1bac01e246e0fe77ee55b742e59ab387287a49
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2026-02-04 16:24:21 +0300

    HW1: task1 - ready for submission

---
+++ src/main/java/ru/mipt/hw1task1/Student.java
@@ -0,0 +1,96 @@
+package ru.mipt.hw1task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents a Student with name and marks
+ */
+
+public class Student {
+    private String name;
+    private List<Integer> marks = new ArrayList<>();
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, List<Integer> marks) {
+        this.name = name;
+        this.marks = new ArrayList<>(marks);
+    }
+
+    /**
+     * Returns this student's name
+     *
+     * @return student's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets this student's name to a given one
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Adds a given mark to this student's marks
+     *
+     * @param mark
+     */
+    public void addMark(int mark) {
+        marks.add(mark);
+    }
+
+    /**
+     * Removes a mark with given index from this student's marks
+     *
+     * @param index
+     */
+    public void removeMark(int index) {
+        if (index < marks.size()) {
+            marks.remove(index);
+        }
+    }
+
+    /**
+     * Returns the mark with given index
+     *
+     * @param index
+     * @return mark with given index
+     */
+    public int getMark(int index) {
+        return marks.get(index);
+    }
+
+    /**
+     * Returns a list with all marks obtained by this student
+     *
+     * @return list with all marks
+     */
+    public List<Integer> getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + marks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Student student)) return false;
+        return Objects.equals(name, student.name) && Objects.equals(marks, student.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, marks);
+    }
+}
---
+++ src/test/java/ru/mipt/hw1task1/StudentTest.java
@@ -0,0 +1,179 @@
+package ru.mipt.hw1task1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentTest {
+    @Test
+    public void ctr_shouldCreateWithName() {
+        String name = "Ivan Ivanov";
+        Student ivan = new Student(name);
+        assertEquals(name, ivan.getName());
+    }
+
+    @Test
+    public void ctr_shouldCreateWithNameAndMarks() {
+        String name = "Peter Petrov";
+        List<Integer> marks = List.of(5, 4, 3, 4, 5);
+        Student peter = new Student(name, marks);
+        assertEquals(name, peter.getName());
+        List<Integer> expectedMarks = List.of(5, 4, 3, 4, 5);
+        assertEquals(expectedMarks, peter.getMarks());
+    }
+
+    @Test
+    public void name_shouldChangeName() {
+        String name = "Svetlana Petrova";
+        Student sveta = new Student(name);
+        String expectedName = "Svetlana Ivanova";
+        sveta.setName(expectedName);
+        assertEquals(expectedName, sveta.getName());
+    }
+
+    @Test
+    public void marks_shouldAddMarkToEmptyList() {
+        String name = "Peter Petrov";
+        Student peter = new Student(name);
+        int expectedMark = 3;
+        peter.addMark(expectedMark);
+        assertEquals(1, peter.getMarks().size());
+        assertEquals(expectedMark, peter.getMark(0));
+    }
+
+    @Test
+    public void marks_shouldAddMarkToNonEmptyList() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3, 4, 5, 2);
+        Student peter = new Student(name, initialMarks);
+        int expectedMark = 3;
+        peter.addMark(expectedMark);
+        assertEquals(initialMarks.size() + 1, peter.getMarks().size());
+        assertEquals(expectedMark, peter.getMark(initialMarks.size()));
+    }
+
+    @Test
+    public void marks_shouldRemoveMarkFromEmptyListWithNoEffect() {
+        String name = "Peter Petrov";
+        Student peter = new Student(name);
+        int someIndex = 5;
+        peter.removeMark(someIndex);
+        assertEquals(0, peter.getMarks().size());
+    }
+
+    @Test
+    public void marks_shouldRemoveMarkFromNonEmptyList() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3, 4);
+        Student peter = new Student(name, initialMarks);
+        peter.removeMark(2);
+        List<Integer> expectedMarks = List.of(5, 4, 4);
+        assertEquals(initialMarks.size() - 1, peter.getMarks().size());
+        assertEquals(expectedMarks, peter.getMarks());
+
+        peter.removeMark(0);
+        expectedMarks = List.of(4, 4);
+        assertEquals(initialMarks.size() - 2, peter.getMarks().size());
+        assertEquals(expectedMarks, peter.getMarks());
+    }
+
+    @Test
+    public void str_shouldProvideToString() {
+        String name = "Peter Petrov";
+        List<Integer> marks = List.of(5, 4, 3, 4, 5, 2);
+        Student peter = new Student(name, marks);
+        String expected = name + ": " + "[5, 4, 3, 4, 5, 2]";
+        assertEquals(expected, peter.toString());
+    }
+
+    @Test
+    public void equals_shouldBeEqual() {
+        String name1 = "Peter Petrov";
+        List<Integer> marks1 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter1 = new Student(name1, marks1);
+
+        String name2 = "Peter Petrov";
+        List<Integer> marks2 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter2 = new Student(name2, marks2);
+
+        String name3 = "Peter Petrov";
+        List<Integer> marks3 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter3 = new Student(name3, marks3);
+
+        // Reflexivity
+        assertTrue(peter1.equals(peter1));
+
+        // Symmetry
+        assertTrue(peter1.equals(peter2));
+        assertTrue(peter2.equals(peter1));
+
+        // Transitivity
+        assertTrue(peter2.equals(peter1));
+        assertTrue(peter3.equals(peter1));
+    }
+
+    @Test
+    public void equals_shouldNotBeEqualWhenNotSameName() {
+        String name1 = "Peter Petro";
+        List<Integer> marks1 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter1 = new Student(name1, marks1);
+
+        String name2 = "Peter Petrov";
+        List<Integer> marks2 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter2 = new Student(name2, marks2);
+
+        assertFalse(peter2.equals(peter1));
+    }
+
+    @Test
+    public void equals_shouldNotBeEqualWhenNotSameMarks() {
+        String name1 = "Peter Petrov";
+        List<Integer> marks1 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter1 = new Student(name1, marks1);
+
+        String name2 = "Peter Petrov";
+        List<Integer> marks2 = List.of(5, 4, 3, 4, 5);
+        Student peter2 = new Student(name2, marks2);
+
+        assertFalse(peter2.equals(peter1));
+    }
+
+    @Test
+    public void equals_shouldNotBeEqualWhenNotStudent() {
+        String name = "Peter Petrov";
+        List<Integer> marks = List.of(5, 4, 3, 4, 5, 2);
+        Student peter = new Student(name, marks);
+
+        assertFalse(peter.equals("Hello World"));
+    }
+
+    @Test
+    public void hash_shouldHaveSameHashCodeWhenEqualObjects() {
+        String name1 = "Peter Petrov";
+        List<Integer> marks1 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter1 = new Student(name1, marks1);
+
+        String name2 = "Peter Petrov";
+        List<Integer> marks2 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter2 = new Student(name2, marks2);
+
+        assertTrue(peter2.equals(peter1));
+        assertEquals(peter2.hashCode(), peter1.hashCode());
+    }
+
+    @Test
+    public void hash_shouldHaveDifferentHashCodeWhenNotEqualObjects() {
+        String name1 = "Peter Petr";
+        List<Integer> marks1 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter1 = new Student(name1, marks1);
+
+        String name2 = "Peter Petrov";
+        List<Integer> marks2 = List.of(5, 4, 3, 4, 5, 2);
+        Student peter2 = new Student(name2, marks2);
+
+        assertFalse(peter2.equals(peter1));
+        assertNotEquals(peter2.hashCode(), peter1.hashCode());
+    }
+}
