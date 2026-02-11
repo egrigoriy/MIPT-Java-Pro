1c18a9bcdfc52cd4fa914b7fe11524ba3d30bb87
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2026-02-11 12:28:48 +0300

    HW1: task3 - ready for first commit

---
+++ src/main/java/ru/mipt/hw1task3/Student.java
@@ -0,0 +1,119 @@
+package ru.mipt.hw1task3;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * Represents a Student with name and marks of type T
+ */
+
+public class Student<T> {
+    private String name;
+    private List<T> marks;
+    private final Predicate<T> markValidityRule;
+    private final List<Consumer<Student<T>>> prevOps;
+
+    public Student(String name) {
+        this(name, List.of());
+    }
+
+    public Student(String name, List<T> marks) {
+        this(name, marks, mark -> true);
+    }
+
+    public Student(String name, List<T> marks, Predicate<T> markValidityRule) {
+        this.name = name;
+        this.marks = new ArrayList<T>(marks);
+        if (!marks.stream().allMatch(markValidityRule)) {
+            throw new IllegalArgumentException("Provided list of marks contains invalid values:" + marks);
+        }
+        this.markValidityRule = markValidityRule;
+        this.prevOps = new ArrayList<>();
+    }
+
+    /**
+     * Returns this student's name
+     *
+     * @return student's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets this student's name to a given one.
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        String oldName = this.name;
+        prevOps.add((student) -> student.name = oldName);
+        this.name = name;
+    }
+
+    /**
+     * Adds a given mark to this student's marks.
+     * Throws InvalidParameterException if the mark doesn't conform to the rule.
+     *
+     * @param mark
+     * @throws InvalidParameterException
+     */
+    public void addMark(T mark) throws InvalidParameterException {
+        if (!markValidityRule.test(mark)) {
+            throw new IllegalArgumentException("The given mark " + mark + " is invalid!");
+        }
+        prevOps.add((student) -> student.marks.remove(marks.size() - 1));
+        marks.add(mark);
+    }
+
+    /**
+     * Removes last mark with provided value. If no such mark do nothing.
+     *
+     * @param value
+     */
+    public void removeMark(T value) {
+        int index = marks.lastIndexOf(value);
+        if (index != -1) {
+            prevOps.add((student) -> student.marks.add(index, value));
+            marks.remove(index);
+        }
+    }
+
+    /**
+     * Returns a list with all marks obtained by this student
+     *
+     * @return list with all marks
+     */
+    public List<T> getMarks() {
+        return marks;
+    }
+
+    /**
+     * Undoes the last action of setting name or adding/removing a mark
+     */
+    public void undo() {
+        if (prevOps.isEmpty()) return;
+
+        Consumer<Student<T>> lastOperation = prevOps.remove(prevOps.size() - 1);
+        lastOperation.accept(this);
+    }
+    @Override
+    public String toString() {
+        return name + ": " + marks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Student student)) return false;
+        return Objects.equals(name, student.name) && Objects.equals(marks, student.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, marks);
+    }
+}
---
+++ src/test/java/ru/mipt/hw1task3/StudentTest.java
@@ -0,0 +1,164 @@
+package ru.mipt.hw1task3;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentTest {
+    @Test
+    public void undo_setName_shouldUndoSingleTime() {
+        String name = "Ivan Ivanov";
+        Student<String> ivan = new Student<>(name);
+        assertEquals(name, ivan.getName());
+        String newName = "Ivanushka";
+        ivan.setName(newName);
+        assertEquals(newName, ivan.getName());
+        ivan.undo();
+        assertEquals(name, ivan.getName());
+    }
+
+    @Test
+    public void undo_setName_shouldUndoMultipleTimes() {
+        String initialName = "Ivan Ivanov";
+        Student<String> ivan = new Student<>(initialName);
+        String newName = "Ivanushka";
+        ivan.setName(newName);
+        String newName2 = "Ivanushka 2";
+        ivan.setName(newName2);
+        ivan.undo();
+        ivan.undo();
+        assertEquals(initialName, ivan.getName());
+    }
+
+    @Test
+    public void undo_setName_shouldUndoTilInitial() {
+        String initialName = "Ivan Ivanov";
+        Student<String> ivan = new Student<>(initialName);
+        String newName = "Ivanushka";
+        ivan.setName(newName);
+        String newName2 = "Ivanushka 2";
+        ivan.setName(newName2);
+        ivan.undo();
+        ivan.undo();
+        ivan.undo();
+        ivan.undo();
+        ivan.undo();
+        assertEquals(initialName, ivan.getName());
+    }
+
+    @Test
+    public void undo_removeMark_shouldUndoSingleTime() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3, 4, 5);
+        Student<Integer> peter = new Student<>(name, initialMarks);
+        peter.removeMark(3);
+        List<Integer> expectedMarksInt = List.of(5, 4, 4, 5);
+        assertEquals(expectedMarksInt, peter.getMarks());
+        peter.undo();
+        assertEquals(initialMarks, peter.getMarks());
+    }
+
+    @Test
+    public void undo_removeMark_shouldUndoMultipleTimes() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3);
+        Student<Integer> peter = new Student<>(name, initialMarks);
+        peter.removeMark(4);
+        peter.removeMark(3);
+        peter.removeMark(5);
+        List<Integer> expectedMarksInt = List.of();
+        assertEquals(expectedMarksInt, peter.getMarks());
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        assertEquals(initialMarks, peter.getMarks());
+    }
+
+    @Test
+    public void undo_removeMark_shouldUndoTilInitial() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3);
+        Student<Integer> peter = new Student<>(name, initialMarks);
+        peter.removeMark(4);
+        peter.removeMark(3);
+        peter.removeMark(5);
+        List<Integer> expectedMarksInt = List.of();
+        assertEquals(expectedMarksInt, peter.getMarks());
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        assertEquals(initialMarks, peter.getMarks());
+    }
+
+    @Test
+    public void undo_addMark_shouldUndoSingleTime() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3, 4, 5);
+        Student<Integer> peter = new Student<>(name, initialMarks);
+        peter.addMark(2);
+        List<Integer> expectedMarksInt = List.of(5, 4, 3, 4, 5, 2);
+        assertEquals(expectedMarksInt, peter.getMarks());
+        peter.undo();
+        assertEquals(initialMarks, peter.getMarks());
+    }
+
+    @Test
+    public void undo_addMark_shouldUndoMultipleTimes() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3);
+        Student<Integer> peter = new Student<>(name, initialMarks);
+        peter.addMark(2);
+        peter.addMark(3);
+        peter.addMark(5);
+        List<Integer> expectedMarksInt = List.of(5, 4, 3, 2, 3, 5);
+        assertEquals(expectedMarksInt, peter.getMarks());
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        assertEquals(initialMarks, peter.getMarks());
+    }
+
+    @Test
+    public void undo_addMark_shouldUndoTilInitial() {
+        String name = "Peter Petrov";
+        List<Integer> initialMarks = List.of(5, 4, 3);
+        Student<Integer> peter = new Student<>(name, initialMarks);
+        peter.addMark(2);
+        peter.addMark(3);
+        peter.addMark(5);
+        List<Integer> expectedMarksInt = List.of(5, 4, 3, 2, 3, 5);
+        assertEquals(expectedMarksInt, peter.getMarks());
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        peter.undo();
+        assertEquals(initialMarks, peter.getMarks());
+    }
+
+    @Test
+    public void undo_mixture_shouldUndoTilInitial() {
+        String name = "Svetlana Ivanova";
+        List<Integer> initialMarks = List.of(5, 4, 3);
+        Student<Integer> sveta = new Student<>(name, initialMarks);
+        sveta.addMark(2);
+        sveta.addMark(3);
+        sveta.setName("Sveta Petrova");
+        sveta.addMark(5);
+        sveta.setName("Sveta Nikolaeva");
+        List<Integer> expectedMarksInt = List.of(5, 4, 3, 2, 3, 5);
+        assertEquals(expectedMarksInt, sveta.getMarks());
+        sveta.undo();
+        sveta.undo();
+        sveta.undo();
+        sveta.undo();
+        sveta.undo();
+        assertEquals(initialMarks, sveta.getMarks());
+        assertEquals(name, sveta.getName());
+    }
+}
