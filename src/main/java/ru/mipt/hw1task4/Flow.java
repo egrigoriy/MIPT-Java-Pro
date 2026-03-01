3903c30a3282c229c54bfd9cb918477a349e9eb5
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2026-03-01 17:50:18 +0300

    WH1Task4: Make Flow with list of operations

--- src/main/java/ru/mipt/hw1task4/Flow.java
+++ src/main/java/ru/mipt/hw1task4/Flow.java
@@ -1,4 +1,88 @@
 package ru.mipt.hw1task4;
 
-public class Flow {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.*;
+
+public class Flow<T> {
+    private List<T> data;
+    List<Runnable> operations = new ArrayList<>();
+
+    private Flow(List<T> list) {
+        if (list == null) throw new NullPointerException("Provided argument is null");
+        data = new ArrayList<>(list);
+    }
+
+    public static <T> Flow<T> of(List<T> list) {
+        return new Flow<>(list);
+    }
+
+    @SafeVarargs
+    public static <T> Flow<T> of(T... args) {
+        return Flow.of(Arrays.asList(args));
+    }
+
+    public static <T> Flow<T> of(T seed, Function<T, T> next, Predicate<T> hasNext) {
+        List<T> list = new ArrayList<>();
+        T val = seed;
+        while(hasNext.test(val)) {
+            list.add(val);
+            val = next.apply(val);
+        }
+        return Flow.of(list);
+    }
+
+    public Flow<T> map(Function<T, T> function) {
+        Runnable runnable = () -> {
+            List<T> result = new ArrayList<>();
+            for (T el : data) {
+                result.add(function.apply(el));
+            }
+            data = result;
+        };
+        operations.add(runnable);
+        return this;
+    }
+
+    public Flow<T> filter(Predicate<T> predicate) {
+        Runnable runnable = () -> {
+            List<T> result = new ArrayList<>();
+            for (var el : data) {
+                if (predicate.test(el)) {
+                result.add(el);
+            }
+        }
+        data = result;
+    };
+    operations.add(runnable);
+    return this;
+}
+
+public T reduce(BinaryOperator<T> operator) {
+    for (Runnable runnable : operations) {
+        runnable.run();
+    }
+    T acc = data.get(0);
+    for (int i = 1; i < data.size(); i++) {
+        acc = operator.apply(acc, data.get(i));
+    }
+    return acc;
+}
+
+public <U, V> Collection<T> collect(Supplier<Collection<T>> supplier, BiConsumer<U, V> combiner) {
+        Collection<T> result = supplier.get();
+        for (T el : data) {
+
+        }
+        return result;
+    }
+
+    public List<T> toList() {
+        for (Runnable runnable : operations) {
+            runnable.run();
+        }
+        return new ArrayList<>(data);
+    }
 }
--- src/test/java/ru/mipt/hw1task4/FlowTest.java
+++ src/test/java/ru/mipt/hw1task4/FlowTest.java
@@ -1,4 +1,73 @@
 package ru.mipt.hw1task4;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FlowTest {
+
+    @Test
+    public void of_List() {
+        List<Integer> list = Arrays.asList(1, -2, 3);
+        Flow<Integer> f = Flow.of(list);
+        List<Integer> expected = List.of(1, -2, 3);
+        assertEquals(expected, f.toList());
+    }
+
+    @Test
+    public void of_VarArgs() {
+        Flow f = Flow.of(5, 4, 0, -1, 99);
+    }
+
+    @Test
+    public void of_Generator() {
+        var f = Flow.of(3, x -> x + 1, x -> x < 10);
+        List<Integer> expected = List.of(3, 4, 5, 6, 7, 8, 9);
+        assertEquals(expected, f.toList());
+    }
+
+    @Test
+    public void map() {
+        List<Integer> list = Arrays.asList(1, -2, 3);
+        List<Integer> result = Flow.of(list).map(x -> x * x).toList();
+        List<Integer> expected = List.of(1, 4, 9);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mapNullThrow() {
+        List<Integer> list = Arrays.asList(1, -2, 3, null);
+        Function<Integer, Integer> f = x -> x * x;
+        assertThrows(NullPointerException.class, () -> Flow.of(list).map(f).toList());
+    }
+
+    @Test
+    public void filter() {
+        List<Integer> list = Arrays.asList(1, -2, 3, -99);
+        List<Integer> result = Flow.of(list).filter(x -> x > 0).toList();
+        List<Integer> expected = List.of(1, 3);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void filterFindNull() {
+        List<Integer> list = Arrays.asList(1, null, 3, null);
+        List<Integer> result = Flow.of(list).filter(Objects::isNull).toList();
+        List<Integer> expected = Arrays.asList(null, null);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMultiple() {
+        int result = Flow.of(1, -2, 3).filter(x -> x > 0).reduce((x, y) -> x + y);
+        int expected = 4;
+        assertEquals(expected, result);
+    }
 }
